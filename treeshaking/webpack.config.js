1b73d42b1602c86754f2f0af5f458f3885a5a086
timeflies <keen.w@outlook.com>
2024-07-07 17:54:20 +0800

    treeshaking: 更新webpack配置

--- treeshaking/webpack.config.js
+++ treeshaking/webpack.config.js
@@ -1,13 +1,26 @@
-const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: {
     demo1: './demo1.js',
-    demo2: './demo2.js'
+    demo2: './demo2.js',
   },
   output: {
     filename: '[name].js',
-    path: __dirname + '/dist'
+    path: __dirname + '/dist',
   },
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    concatenateModules: false,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: false,
+        terserOptions: {
+          mangle: false,
+          format: { beautify: true },
+        },
+      }),
+    ],
+  },
 }
